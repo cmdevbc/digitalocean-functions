a479441401c64eec17bbc51321b7fae6dbd632cd
cmdevbc <89685615+cmdevbc@users.noreply.github.com>
2023-02-14 20:30:26 +0300

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,8 +1,22 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
-  }
+const MongoClient = require('mongodb').MongoClient;
 
-exports.main = main
+async function main() {
+    const uri = process.env['MONGODB'];
+    let client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const data = await client.db("admin").collection("prices").find().limit(5);
+        return { data };
+    } catch (e) {
+        console.error(e);
+        return {
+            "body": { "error": "There was a problem adding the data to the database." },
+            "statusCode": 400
+        };
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports.main = main;
